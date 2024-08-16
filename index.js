ce49e9a68df1675678533e565d681e05de7e0237
Praveen s <166095477+Praveensiva2004@users.noreply.github.com>
2024-08-16 16:47:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+    
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Initialize the slider
+showSlide(currentSlide);
